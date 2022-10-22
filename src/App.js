f740ca5aea1b208fa6673b5353bf7cb27d6fb854
JKmoises <moi.prado@duocuc.cl>
2022-10-22 17:42:44 -0300

    Maquetación de app completada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { InputTask } from "./components/InputTask";
+import { TaskList } from "./components/TaskList";
 
 function App() {
   return (
     <div className="container">
       <h1 className="section-title">Listado de tareas</h1>
+      <InputTask />
+      <TaskList />
     </div>
   );
 }
