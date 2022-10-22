580c34a90b21468a1f4c8373be9f3a354d1867c0
JKmoises <moi.prado@duocuc.cl>
2022-10-22 17:42:44 -0300

    Maquetación de app completada

---
+++ src/components/InputTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const InputTask = () => {
+  return (
+    <div className="input-task-container">
+      <input type="text" className="input-task" />
+      <button className="btn-task">Crear Tarea</button>
+    </div>
+  );
+}
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,47 @@
+
+export const TaskList = () => {
+  return (
+    // <i class="fas fa-user-check"></i>
+    <section className="task-list">
+      <article className="task section box-shadow-1">
+        <p className="task-name">Terminar Libro</p>
+
+        <span className="task-state" title="Terminar tarea">
+          <i className="fas fa-user-clock"></i>
+        </span>
+
+        <div className="acciones">
+          <button className="btn-editar">Editar</button>
+          <button className="btn-eliminar">Eliminar</button>
+        </div>
+      </article>
+
+      <article className="task section box-shadow-1">
+        <p className="task-name">Terminar Libro</p>
+
+        <span className="task-state" title="Terminar tarea">
+          <i className="fas fa-user-clock"></i>
+        </span>
+
+        <div className="acciones">
+          <button className="btn-editar">Editar</button>
+          <button className="btn-eliminar">Eliminar</button>
+        </div>
+      </article>
+
+      <article className="task section box-shadow-1">
+        <p className="task-name">Terminar Libro</p>
+
+        <span className="task-state" title="Terminar tarea">
+          <i className="fas fa-user-clock"></i>
+          
+        </span>
+
+        <div className="acciones">
+          <button className="btn-editar">Editar</button>
+          <button className="btn-eliminar">Eliminar</button>
+        </div>
+      </article>
+    </section>
+  );
+}
