38fd7efc10ae40e42edeff262c66dc7d1ce9e7d7
JKmoises <moi.prado@duocuc.cl>
2022-10-29 14:51:53 -0300

    Agregando validaciones al actualizar una tarea

--- src/components/TaskUpdater.jsx
+++ src/components/TaskUpdater.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Swal from "sweetalert2/dist/sweetalert2.js";
 
 export const TaskUpdater = ({ task, updateTask,setDataToEdit}) => {
   const [taskForm, setTaskForm] = useState(task);
@@ -12,11 +13,58 @@ export const TaskUpdater = ({ task, updateTask,setDataToEdit}) => {
 
   const handleKeyDown = (e) => { 
     if (e.key !== "Enter") return;
+    e.preventDefault();
+
+    if (taskForm.name === "") {
+      Swal.fire({
+        title: "No has agregado una tarea",
+        text: "Debes agregar una tarea",
+        timer: 3000,
+        icon: "error",
+      });
+      return;
+    }
+
+    if (taskForm.name.length > 25) {
+      Swal.fire({
+        title: "La descripción de la tarea es muy larga",
+        text: "La tarea debe tener hasta 25 caracteres",
+        icon: "info",
+        timer: 3000,
+        customClass: {
+          title: "fs-20-8",
+        },
+      });
+      return;
+    }
     handleUpdateTask();
 
   }
   
   const handleUpdateTask = () => {
+    if (taskForm.name === "") {
+      Swal.fire({
+        title: "No has agregado una tarea",
+        text: "Debes agregar una tarea",
+        timer: 3000,
+        icon: "error",
+      });
+      return;
+    }
+
+    if (taskForm.name.length > 25) {
+      Swal.fire({
+        title: "La descripción de la tarea es muy larga",
+        text: "La tarea debe tener hasta 25 caracteres",
+        icon: "info",
+        timer: 3000,
+        customClass: {
+          title: "fs-20-8",
+        },
+      });
+      return;
+    }
+    
     setDataToEdit(false);
     updateTask(taskForm);
   }
